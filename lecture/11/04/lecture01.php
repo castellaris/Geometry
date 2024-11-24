d60ca191b1942791769c93d7c00567903f454f75
Константин <156214293+castellaris@users.noreply.github.com>
2024-11-24 19:25:25 +0500

    Add files via upload

---
+++ lecture/11/04/lecture01.php
@@ -0,0 +1,133 @@
+<?php include $_SERVER['DOCUMENT_ROOT'] . '/Geometry/includes/header.php'; ?>
+    <?php include $_SERVER['DOCUMENT_ROOT'] . '/Geometry/includes/navigation.php'; ?>
+    <body>
+    <div class="container" style="padding: 5px">
+    <div class="row justify-content-center">
+        <a href="/Geometry/lecture/index.php"><h2>Лекции</h2></a>			
+    </div>
+    <div class="row justify-content-center" style="width: 100%;">
+        <a href="/Geometry/lecture/10/01/index.php"><h3>11 класс. Сложные геометрические фигуры</h3></a>				
+    </div>
+    <div class="row justify-content-center" style="margin: 5px">
+        <h4>Сложные геометрические фигуры</h4>			
+    </div>
+    <div class="row justify-content-center" style="margin: 5px">
+        <h4>Тема: Сложные геометрические фигуры</h4>			
+    </div>
+    <div class="text-right">
+        <input class="place_for_search" type="text" id="text-to-find" value="" placeholder="Поиск" autofocus> 			
+        <button class="btn btn-outline-danger"><img src="/Geometry/images/stop.png" alt="Отменить" title="Отменить поиск" onclick="javascript: FindOnPage('text-to-find',false); return false;" style="vertical-align: middle"></button>
+        <button class="btn btn-outline-info"><img src="/Geometry/images/search.png" alt="Поиск" title="Начать поиск" onclick="javascript: FindOnPage('text-to-find',true); return false;" style="vertical-align: middle"></button>
+    <div style="margin-top:25px;">		
+        <div class=WordSection1> 
+            <!-- Ваш контент здесь -->
+        </div>
+    </div>
+</div>
+
+    <main>
+        <section style="margin-top: 20px; margin-bottom: 20px; text-align: center;">
+            <h2 style="margin-top:0cm; margin-bottom:0cm; background:white;">
+                <span style="font-family:'Helvetica',sans-serif; color:#333333;">
+                    <a href="https://interneturok.ru/lesson/geometry/10-klass/vektory-v-prostranstve/komplanarnye-vektory?block=content#mediaplayer" title="Смотреть в видеоуроке">
+                        <b><span style="color:#346BA2; text-decoration:none">Введение в сложные геометрические тела</span></b>
+                    </a>
+                </span>
+            </h2>
+            <p style="margin-top:15.0pt; margin-right:0cm; margin-bottom:0cm; margin-left:0cm; margin-bottom:.0001pt; background:white; width:auto;">
+                <em><span style="font-size:11.5pt; font-family:'Helvetica',sans-serif; color:#333333">Сложные геометрические тела представляют собой объекты, состоящие из комбинаций многогранников и тел вращения. Они имеют разнообразные формы и являются важными объектами в геометрии и ее приложениях. </span></em>
+            </p>
+        </section>
+        <section style="margin-top: 20px; margin-bottom: 20px; text-align: center;">
+            <h2 style="margin-top:0cm; margin-bottom:0cm; background:white;">
+                <span style="font-family:'Helvetica',sans-serif; color:#333333;">
+                    <a href="https://interneturok.ru/lesson/geometry/10-klass/vektory-v-prostranstve/komplanarnye-vektory?block=content#mediaplayer" title="Смотреть в видеоуроке">
+                        <b><span style="color:#346BA2; text-decoration:none">Комбинации многогранников и тел вращения</span></b>
+                    </a>
+                </span>
+            </h2>
+            <p style="margin-top:15.0pt; margin-right:0cm; margin-bottom:0cm; margin-left:0cm; margin-bottom:.0001pt; background:white; width:auto;">
+                <em><span style="font-size:11.5pt; font-family:'Helvetica',sans-serif; color:#333333">Сложные тела могут быть построены из различных многогранников, таких как кубы, параллелепипеды, призмы, а также из тел вращения, таких как цилиндры, конусы и шары. Комбинация этих элементов создает уникальные и интересные формы. Формула нахождения объема куба: V=πr**2 h, где V-объем куба. Формула нахождения объема параллелепипеда: V= l * w * h, где l-длина, w-ширина, h-высота параллелепипеда. Формула нахождения объема призмы: V = Aосн * h, Где Aосн-площадь основания призмы, h-высота призмы. Формула нахождения объема цилиндра: V = πr**2h, где r=радиус основания, h - высота цилиндра. Формула нахождения объема конуса: V = 4/3πr**2h, где r-радиус основания, h-высота конуса. Формула нахождения объема шара, V = 4/3πr**3, где r - радиус шара</span></em>
+                <img src="https://resh.edu.ru/uploads/lesson_extract/4911/20190201121516/OEBPS/objects/t_geom_11_15_13/5bfd7710716ca79723b83378.png" alt="">
+            </p>
+        </section>
+        <section style="margin-top: 20px; margin-bottom: 20px; text-align: center;">
+            <h2 style="margin-top:0cm; margin-bottom:0cm; background:white;">
+                <span style="font-family:'Helvetica',sans-serif; color:#333333;">
+                    <a href="https://interneturok.ru/lesson/geometry/10-klass/vektory-v-prostranstve/komplanarnye-vektory?block=content#mediaplayer" title="Смотреть в видеоуроке">
+                        <b><span style="color:#346BA2; text-decoration:none">Сечения сложных фигур</span></b>
+                    </a>
+                </span>
+            </h2>
+            <p style="margin-top:15.0pt; margin-right:0cm; margin-bottom:0cm; margin-left:0cm; margin-bottom:.0001pt; background:white; width:auto;">
+                <em><span style="font-size:11.5pt; font-family:'Helvetica',sans-serif; color:#333333">При прохождении плоскости через сложные геометрические тела образуются сечения, которые могут иметь различные формы и свойства. Изучение сечений помогает понять внутреннее строение и структуру сложных тел. Формулы сечения сложных фигур включают:
+
+Цилиндр: Площадь сечения цилиндра, перпендикулярного его оси, равна площади круга: 
+𝑆
+=
+𝜋
+𝑟
+2
+S=πr 
+2
+ , где 
+𝑟
+r - радиус цилиндра. При сечении плоскостью, параллельной оси, образуется эллипс. Площадь этого сечения: 
+𝑆
+=
+𝜋
+𝑎
+𝑏
+S=πab, где 
+𝑎
+a и 
+𝑏
+b - полуоси эллипса.
+
+Конус: При сечении плоскостью, перпендикулярной оси, образуется круг: 
+𝑆
+=
+𝜋
+𝑟
+2
+S=πr 
+2
+ , где 
+𝑟
+r - радиус основания конуса. Сечение плоскостью, параллельной основанию, образует меньший конус, подобный исходному.
+
+Пирамида: При сечении плоскостью, перпендикулярной основанию, образуется многоугольник, площадь которого можно вычислить стандартными методами. Сечение плоскостью, параллельной основанию, образует меньшую подобную пирамиду.</span></em>
+                <img src="https://shkolkovo.net/media/upload/task_images/1342/1r.png" alt="">
+            </p>
+        </section>
+        <section style="margin-top: 20px; margin-bottom: 20px; text-align: center;">
+            <h2 style="margin-top:0cm; margin-bottom:0cm; background:white;">
+                <span style="font-family:'Helvetica',sans-serif; color:#333333;">
+                    <a href="https://interneturok.ru/lesson/geometry/10-klass/vektory-v-prostranstve/komplanarnye-vektory?block=content#mediaplayer" title="Смотреть в видеоуроке">
+                        <b><span style="color:#346BA2; text-decoration:none">Взаимное расположение фигур в пространстве</span></b>
+                    </a>
+                </span>
+            </h2>
+            <p style="margin-top:15.0pt; margin-right:0cm; margin-bottom:0cm; margin-left:0cm; margin-bottom:.0001pt; background:white; width:auto;">
+                <em><span style="font-size:11.5pt; font-family:'Helvetica',sans-serif; color:#333333">Сложные геометрические тела могут находиться в различных взаимных положениях в трехмерном пространстве, таких как соприкосновение, пересечение, вложение и т.д. Изучение взаимного расположения фигур важно для анализа и моделирования различных объектов. Для этого применяются различные математические концепции и формулы.
+
+Расстояние между двумя точками в трехмерном пространстве вычисляется по формуле \( d = \sqrt{(x_2 - x_1)^2 + (y_2 - y_1)^2 + (z_2 - z_1)^2} \). Уравнение плоскости в трехмерном пространстве задается формулой \(Ax + By + Cz + D = 0\), где \(A\), \(B\), \(C\) и \(D\) - константы, определяющие параметры плоскости.
+
+Для описания прямой в трехмерном пространстве используется параметрическая форма, например: \( x = x_0 + at \), \( y = y_0 + bt \), \( z = z_0 + ct \). Здесь \( (x_0, y_0, z_0) \) - координаты точки на прямой, \( a \), \( b \), \( c \) - направляющие коэффициенты прямой, а \( t \) - параметр.
+
+Угол между двумя векторами \( \vec{a} \) и \( \vec{b} \) вычисляется по формуле \( \cos(\theta) = \frac{\vec{a} \cdot \vec{b}}{|\vec{a}| |\vec{b}|} \), где \( \vec{a} \cdot \vec{b} \) - скалярное произведение векторов, а \( |\vec{a}| \) и \( |\vec{b}| \) - их длины.
+
+Эти формулы могут быть использованы для определения взаимного расположения различных геометрических фигур в трехмерном пространстве, таких как прямые, плоскости, точки и т. д.</span></em>
+                <img src="https://resolventa.ru/sprris/stereometry/pp/pp5.png" alt="">
+            </p>
+        </section>
+    </main>
+		<div text-align="center" class="justify-content-center">
+			<a style="align:center" href="javascript:history.back();"><?php echo $Lang['back'];?></a>	
+			<a>|</a>
+			<a style="align:center" href="javascript:scroll(0,0)"><?php echo $Lang['page_up'];?></a>	
+		</div>				
+	</div>	<!-- /container -->
+</body>
+    <?php include $_SERVER['DOCUMENT_ROOT'] . '/Geometry/includes/footer.php'; ?>
+</html>
