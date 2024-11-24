6898af93e591f73a3a24383a05ff7a12e7a545fa
Константин <156214293+castellaris@users.noreply.github.com>
2024-11-24 19:14:50 +0500

    Add files via upload

---
+++ lecture/10/05/lecture01.php
@@ -0,0 +1,127 @@
+<?php 
+include $_SERVER['DOCUMENT_ROOT'] . '/Geometry/includes/header.php'; 
+include $_SERVER['DOCUMENT_ROOT'] . '/Geometry/includes/navigation.php'; 
+?> 
+
+<div class="container" style="padding: 5px">
+    <div class="row justify-content-center" style="width: 100%;">
+        <a href="/Geometry/lecture/index.php"><h2>Лекции</h2></a>			
+    </div>
+    <div class="row justify-content-center" style="width:100%;">
+        <a href="/Geometry/lecture/10/01/index.php"><h3>10 класс. Окружность. Построение окружностей и отрезков с заданными свойствами</h3></a>				
+    </div>
+    <div class="row justify-content-center" style="margin: 5px">
+        <h4>Окружность, уравнения окружности</h4>			
+    </div>
+    <div class="row justify-content-center" style="margin: 5px">
+        <h4>Тема: Окружность</h4>			
+    </div>
+    <div class="text-right">
+        <input class="place_for_search" type="text" id="text-to-find" value="" placeholder="Поиск" autofocus> 			
+        <button class="btn btn-outline-danger"><img src="/Geometry/images/stop.png" alt="Отменить" title="Отменить поиск" onclick="javascript: FindOnPage('text-to-find',false); return false;" style="vertical-align: middle"></button>
+        <button class="btn btn-outline-info"><img src="/Geometry/images/search.png" alt="Поиск" title="Начать поиск" onclick="javascript: FindOnPage('text-to-find',true); return false;" style="vertical-align: middle"></button>
+    <div style="margin-top:25px;">		
+        <div class=WordSection1> 
+            <!-- Ваш контент здесь -->
+        </div>
+    </div>
+</div>
+<main style="text-align: center;">
+    <section style="margin-bottom: 20px;">
+        <h2 style='text-align:center; margin-top:0cm; text-align:center; background:white'>
+            <span style='font-family:"Helvetica",sans-serif; color:#333333'>
+                <a href="https://interneturok.ru/lesson/geometry/10-klass/vektory-v-prostranstve/komplanarnye-vektory?block=content#mediaplayer" title="Смотреть в видеоуроке">
+                    <b><span style='color:#346BA2; text-decoration:none'>Введение, понятие компланарности векторов</span></b>
+                </a>
+            </span>
+        </h2>
+        <p style='margin-top:15.0pt; margin-right:0cm; margin-bottom:0cm; margin-left:0cm; margin-bottom:.0001pt; background:white; text-align: center; width:auto;'>
+            <em><span style='font-size:11.5pt; font-family:"Helvetica",sans-serif; color:#333333'>Окружность - одна из самых важных и интересных геометрических фигур. Она играет ключевую роль в различных областях математики и ее свойства широко применяются в реальной жизни, начиная от инженерии и архитектуры и заканчивая физикой и астрономией.</span></em>
+        </p>
+    </section>
+    <section style="margin-top: 20px; margin-bottom: 20px;">
+        <h2 style='text-align:center; margin-top:0cm; text-align:center; background:white; width:auto;'>
+            <span style='font-family:"Helvetica",sans-serif; color:#333333'>
+                <a href="https://interneturok.ru/lesson/geometry/10-klass/vektory-v-prostranstve/komplanarnye-vektory?block=content#mediaplayer" title="Смотреть в видеоуроке">
+                    <b><span style='color:#346BA2; text-decoration:none'>Уравнение окружности</span></b>
+                </a>
+            </span>
+        </h2>
+        <p style='margin-top:15.0pt; margin-right:0cm; margin-bottom:0cm; margin-left:0cm; margin-bottom:.0001pt; background:white; text-align: center;width:auto;'>
+            <em><span style='font-size:11.5pt; font-family:"Helvetica",sans-serif; color:#333333'>Окружность - это множество всех точек, равноудаленных от фиксированной точки, называемой центром. Уравнение окружности имеет вид: (x - h)<sup>2</sup> + (y - k)<sup>2</sup> = r<sup>2</sup>, где (h, k) - координаты центра, а r - радиус окружности.</span></em>
+        </p>
+    </section>
+    <section style="margin-top: 20px; margin-bottom: 20px;">
+        <h2 style='text-align:center; margin-top:0cm; text-align:center; background:white; width:auto;'>
+            <span style='font-family:"Helvetica",sans-serif; color:#333333'>
+                <a href="https://interneturok.ru/lesson/geometry/10-klass/vektory-v-prostranstve/komplanarnye-vektory?block=content#mediaplayer" title="Смотреть в видеоуроке">
+                    <b><span style='color:#346BA2; text-decoration:none'>Касательные к окружности</span></b>
+                </a>
+            </span>
+        </h2>
+        <p style='margin-top:15.0pt; margin-right:0cm; margin-bottom:0cm; margin-left:0cm; margin-bottom:.0001pt; background:white; text-align: center; width:100%;'>
+            <em><span style='font-size:11.5pt; font-family:"Helvetica",sans-serif; color:#333333 ; width:auto;'>Касательная к окружности - это прямая, которая касается окружности в одной и только одной точке. Главное свойство касательной - она перпендикулярна радиусу, проведенному к точке касания.</span></em>
+        <img src="https://ege-study.ru/wp-content/uploads/2012/08/kasat_k_okr_01.png" alt="">
+        </p>
+    </section>
+    <section style="margin-top: 20px; margin-bottom: 20px;">
+        <h2 style='text-align:center; margin-top:0cm; text-align:center; background:white; width:auto;'>
+            <span style='font-family:"Helvetica",sans-serif; color:#333333'>
+                <a href="https://interneturok.ru/lesson/geometry/10-klass/vektory-v-prostranstve/komplanarnye-vektory?block=content#mediaplayer" title="Смотреть в видеоуроке">
+                    <b><span style='color:#346BA2; text-decoration:none'>Вписанные и описанные углы</span></b>
+                </a>
+            </span>
+        </h2>
+        <p style='margin-top:15.0pt; margin-right:0cm; margin-bottom:0cm; margin-left:0cm; margin-bottom:.0001pt; background:white; text-align: center; width:auto;'>
+            <em><span style='font-size:11.5pt; font-family:"Helvetica",sans-serif; color:#333333'>Вписанный угол - это угол, вершина которого лежит на окружности, а стороны проходят через две разные дуги окружности. Описанный угол - это угол, вершина которого находится на окружности, а стороны проходят через хорду (отрезок, соединяющий две точки окружности) и дугу, ограниченную этой хордой.</span></em>
+            <img src="https://egemaximum.ru/wp-content/uploads/2013/05/vpisannie-ugli40.jpg" alt="">
+        </p>
+    </section>
+    <section style="margin-top: 20px; margin-bottom: 20px;">
+        <h2 style='text-align:center; margin-top:0cm; text-align:center; background:white'>
+            <span style='font-family:"Helvetica",sans-serif; color:#333333'>
+                <a href="https://interneturok.ru/lesson/geometry/10-klass/vektory-v-prostranstve/komplanarnye-vektory?block=content#mediaplayer" title="Смотреть в видеоуроке">
+                    <b><span style='color:#346BA2; text-decoration:none'>Теоремы об углах в окружности</span></b>
+                </a>
+            </span>
+        </h2>
+        <p style='margin-top:15.0pt; margin-right:0cm; margin-bottom:0cm; margin-left:0cm; margin-bottom:.0001pt; background:white; text-align: center; width:auto;'>
+            <em><span style='font-size:11.5pt; font-family:"Helvetica",sans-serif; color:#333333'>Теорема о центральном угле - центральный угол, составленный на дуге, равен половине меры этой дуги. Теорема о вписанном угле - вписанный угол равен половине суммы мер дуг, ограниченных этим углом. Теорема о касательной и хорде - угол между касательной и хордой, проведенной из точки касания, равен углу, стоящему на дуге, ограниченной этой хордой.</span></em>
+        </p>
+        <img src="https://lh3.googleusercontent.com/proxy/xaRczOhcs2mEBaYKWUSjf9O7W5oWHCGWau6DMPPZSAvpDhBLUb9vDt0bRuszLwYdo5v8qRMUJLPPdpEUYB6AwlkCOAtH9dFmTAWW" alt="">
+    </section>
+    <section style="margin-top: 20px; margin-bottom: 20px;">
+        <h2 style='text-align:center; margin-top:0cm; text-align:center; background:white'>
+            <span style='font-family:"Helvetica",sans-serif; color:#333333'>
+                <a href="https://interneturok.ru/lesson/geometry/10-klass/vektory-v-prostranstve/komplanarnye-vektory?block=content#mediaplayer" title="Смотреть в видеоуроке">
+                    <b><span style='color:#346BA2; text-decoration:none'>Построение окружностей и отрезков с заданными свойствами</span></b>
+                </a>
+            </span>
+        </h2>
+        <p style='margin-top:15.0pt; margin-right:0cm; margin-bottom:0cm; margin-left:0cm; margin-bottom:.0001pt; background:white; text-align: center; width:auto;'>
+            <em><span style='font-size:11.5pt; font-family:"Helvetica",sans-serif; color:#333333'>Построение окружности по радиусу - для построения окружности с заданным радиусом нужно взять циркуль и вписать его в точку, которая будет центром, а затем описать окружность. Построение окружности по трем точкам - для этого нужно найти серединный перпендикуляр к отрезку, соединяющему две из трех заданных точек, который будет проходить через центр окружности. Построение отрезка равного заданной длине - это можно сделать при помощи циркуля и линейки, измерив нужную длину и отметив ее на линейке, а затем провести отрезок с помощью линейки.</span></em>
+        </p>
+        <img src="https://lh3.googleusercontent.com/proxy/1sRYt7L6nd0Lr2WhrWys8s0Q3FydHM17aWfWKM3cSh9i0ow2THYXW-IkaGkr-HVpudv-w5brTI0vV-x95RfVxA" alt="">
+    </section>
+    <section style="margin-top: 20px; margin-bottom: 20px;">
+        <h2 style='text-align:center; margin-top:0cm; text-align:center; background:white'>
+            <span style='font-family:"Helvetica",sans-serif; color:#333333'>
+                <a href="https://interneturok.ru/lesson/geometry/10-klass/vektory-v-prostranstve/komplanarnye-vektory?block=content#mediaplayer" title="Смотреть в видеоуроке">
+                    <b><span style='color:#346BA2; text-decoration:none'>Заключение</span></b>
+                </a>
+            </span>
+        </h2>
+        <p style='margin-top:15.0pt; margin-right:0cm; margin-bottom:0cm; margin-left:0cm; margin-bottom:.0001pt; background:white; text-align: center; width:auto;'>
+            <em><span style='font-size:11.5pt; font-family:"Helvetica",sans-serif; color:#333333'>Окружность - это фигура с множеством уникальных и интересных свойств, которые можно применить в различных задачах. Понимание этих свойств и умение работать с ними не только развивает геометрическое мышление, но и помогает решать практические задачи из различных областей науки и техники.</span></em>
+        </p>
+    </section>
+</main>
+<div text-align="center" class="justify-content-center">
+			<a style="align:center" href="javascript:history.back();"><?php echo $Lang['back'];?></a>	
+			<a>|</a>
+			<a style="align:center" href="javascript:scroll(0,0)"><?php echo $Lang['page_up'];?></a>	
+		</div>		
+</div>
+</body>
+<?php include $_SERVER['DOCUMENT_ROOT'] . '/Geometry/includes/footer.php'; ?>
+</html>
